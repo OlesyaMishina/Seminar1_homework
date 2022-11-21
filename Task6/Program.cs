9f67559ba563aa5b3b6ac28e7aa06eb47df33e64
Olesya Mishina <olesyaavilova33@gmail.com>
2022-11-21 22:24:03 +0300

    Задача 6: определение четности числа.

---
+++ Task6/Program.cs
@@ -0,0 +1,6 @@
+﻿// программa принимает на вход число и выдаёт, является ли оно четный:
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+double result=num%2.0;
+if (result == 0) Console.Write ($"Число {num} четное.");
+else Console.Write ($"Число {num} нечетное.");
