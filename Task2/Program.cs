c4db0fe6a1ca5c3ebf6cba45870c4ce8090c3157
Olesya Mishina <olesyaavilova33@gmail.com>
2022-11-21 21:21:26 +0300

    Задача 2: определение максимального числа из двух

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// программа на вход принимает два числа и выдаёт, какое число большее, а какое меньшее:
+Console.WriteLine("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 < num2)
+{
+    Console.WriteLine($" max={num2}, min={num1}");
+}
+else
+{
+    Console.WriteLine($" max={num1}, min={num2}");
+}
