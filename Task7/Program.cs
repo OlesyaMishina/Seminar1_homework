911d11f04fc0160e9562a6b689e2dbc76480a928
Olesya Mishina <olesyaavilova33@gmail.com>
2022-11-22 10:58:01 +0300

    задача 7, выводит третью цифру введенного числа.

---
+++ Task7/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 7, выводит третью цифру введенного числа.
+Console.WriteLine("Введите целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num = Math.Abs(num1);
+if (num < 100) 
+{
+    Console.Write($"У числа {num1} отсутствует третья цифра");
+}
+else
+{
+    while (num >= 1000)
+    {
+        int result = num % 10; 
+        num = (num - result)/10;
+    }
+    int result2 = num % 10;
+    Console.Write($"Третьей цифрой числа {num1} является {result2}");
+}
