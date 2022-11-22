d1202b2ae92551ccb627e9daf301bb00fbe09069
Olesya Mishina <olesyaavilova33@gmail.com>
2022-11-22 10:21:31 +0300

    Задача 8: Выводит четные числа от 1 до введенного числа

---
+++ Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿// программa принимает на вход число и выдаёт все четные числа от 1 до введенного числа:
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= num)
+{
+    double result = count % 2.0;
+    if (result == 0) Console.Write($" {count} ");
+    count++;
+}
+    
